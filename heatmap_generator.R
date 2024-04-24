82e391a8e2849b1e261c5c2ef5d30ac394a34712
doemily <111350013+doemily@users.noreply.github.com>
2024-04-24 15:39:14 -0700

    Updated heatmap

--- heatmap_generator.R
+++ heatmap_generator.R
@@ -1,25 +1,31 @@
 library(shiny)
-library(heatmaply)
+library(pheatmap)
+library(dplyr)
 
 # Define UI
 ui <- fluidPage(
   titlePanel("Heatmap Generator"),
   sidebarLayout(
     sidebarPanel(
-      fileInput("file1", "Choose CSV File",
-                accept = c(
-                  "text/csv",
-                  "text/comma-separated-values,text/plain",
-                  ".csv")
-      ),
-      tags$hr(),
-      checkboxInput("header", "Header", TRUE),
-      actionButton('getHmap', 'Generate Heatmap')
+      # Tabs for different sections
+      tabsetPanel(
+        tabPanel("Generate Heatmap",
+                 fileInput("file1", "Choose CSV File",
+                           accept = c(
+                             "text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")
+                 ),
+                 tags$hr(),
+                 checkboxInput("header", "Header", TRUE),
+                 actionButton('getHmap', 'Generate Heatmap')
+        ),
+      )
     ),
     mainPanel(
       tabsetPanel(
         tabPanel("Heatmap",
-                 plotlyOutput("themap")),
+                 plotOutput("themap")),
         tabPanel("Heatmap Table",
                  tableOutput("table.output"))
       )
@@ -29,7 +35,7 @@ ui <- fluidPage(
 
 # Server logic
 server <- function(input, output, session) {
-  data <- reactiveVal(NULL) # Stores the CSV data
+  data <- reactiveVal(NULL)
   
   # Read and return CSV data
   observeEvent(input$file1, {
@@ -39,7 +45,7 @@ server <- function(input, output, session) {
     }
   })
   
-  # Renders CSV as table
+  # Render CSV as table
   output$table.output <- renderTable({
     data()
   })
@@ -47,19 +53,26 @@ server <- function(input, output, session) {
   # Process data for heatmap
   plotdata <- eventReactive(input$getHmap, {
     if (!is.null(data())) {
-      # Select all gene columns dynamically
-      genes <- grep("^Gene\\.\\d+", names(data()), value = TRUE)
-      heatmap_data <- cbind(data()[genes], data()[["Cell"]])
-      colnames(heatmap_data) <- c(genes, "Cell")
-      heatmap_data[is.na(heatmap_data)] <- 0 
-      heatmaply(heatmap_data)
+      clustered_data <- data() %>%
+        group_by(Celltype) %>%
+        summarise(across(-Barcode, sum)) %>%
+        ungroup()
+      
+      rownames(clustered_data) <- clustered_data$Celltype
+      clustered_data <- clustered_data[, -1]
+      clustered_data[is.na(clustered_data)] <- 0
+      clustered_data
     }
   })
   
   # Render heatmap
-  output$themap <- renderPlotly({ 
-    plotdata()
+  output$themap <- renderPlot({ 
+    if (!is.null(plotdata())) {
+      pheatmap(plotdata())
+    }
   })
+  
+  
 }
 
 # Run the application
